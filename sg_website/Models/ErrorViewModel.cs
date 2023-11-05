de370ae2a7c4b6f3136c7d3d8e3a853849f39694
SelmaEdenståhl <SelmaEdensthl@DESKTOP-5F3A5R1>
2023-11-05 21:22:54 +0100

    Created sg website project

---
+++ sg_website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sg_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
